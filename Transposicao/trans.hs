484d26691a248b92a63e810a12893bb2d257b749
Iago Floriano <iagomfloriano@gmail.com>
2022-11-16 16:23:21 -0300

    Transposição com matriz NxN

---
+++ Transposicao/trans.hs
@@ -0,0 +1,26 @@
+import qualified Data.Char as C
+import qualified Data.List as L
+
+fazTabela :: Int -> String -> [String]
+fazTabela n str
+  | length str < n = [take n $ str ++ cycle "X"]
+  | otherwise = take n str : fazTabela n (drop n str)
+
+unMatrix :: [String] -> String
+unMatrix [] = []
+unMatrix (x:xs) = x ++ unMatrix xs
+
+criptografa :: Int -> String -> String
+criptografa k str = unMatrix $ L.transpose $ fazTabela k str
+
+doChosen :: [String] -> String
+doChosen input
+  | length input < 2 = "Entrada fora do padrão\n"
+  | otherwise =
+    criptografa (read k) (filter (\x -> not $ C.isSpace x) $ unwords str) ++
+    "\n"
+  where
+    (k:str) = input
+
+main :: IO ()
+main = interact $ doChosen . words . map C.toUpper
