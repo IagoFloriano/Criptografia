05496f817b17adfedd40c9a13b4b8be71c5de903
Iago Floriano <iagomfloriano@gmail.com>
2022-12-14 19:04:12 -0300

    Ta pronto

--- T1/criptografa.hs
+++ T1/criptografa.hs
@@ -1,8 +1,17 @@
 import qualified Data.Char as C
 import qualified Data.List as L
 import qualified Data.Map as M
+import qualified PlayFair as PF
 import qualified Util as U
+import qualified Viegenere as V
+
+encript :: [String] -> String
+encript input
+  | length input < 3 =
+    "Entrada dada de forma incorreta\n deve ser: [chave play fair] [chave viegenere] [texto a ser criptografado]"
+  | otherwise = V.encript kvg $ PF.encript kpf $ unwords str
+  where
+    (kpf:kvg:str) = input
 
 main :: IO ()
-main = do
-  putStrLn "Saída"
+main = interact $ encript . words . map C.toUpper
--- T1/descriptografa.hs
+++ T1/descriptografa.hs
@@ -1,8 +1,17 @@
 import qualified Data.Char as C
 import qualified Data.List as L
 import qualified Data.Map as M
+import qualified PlayFair as PF
 import qualified Util as U
+import qualified Viegenere as V
+
+decript :: [String] -> String
+decript input
+  | length input < 3 =
+    "Entrada dada de forma incorreta\n deve ser: [chave play fair] [chave viegenere] [texto a ser descriptografado]"
+  | otherwise = PF.decript kpf $ V.decript kvg $ unwords str
+  where
+    (kpf:kvg:str) = input
 
 main :: IO ()
-main = do
-  putStrLn "Saída"
+main = interact $ decript . words . map C.toUpper
