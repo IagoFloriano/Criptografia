14e98527247a52943eb3aa236b48c6b2d1c7caca
guipateiro <guilhermepateiro@gmail.com>
2022-12-16 03:14:47 -0300

    alteracoes no playfair

--- T1/playFair.hs
+++ T1/playFair.hs
@@ -23,15 +23,15 @@ toPairs (x:[]) = [x : "X"]
 
 subs1 :: U.Table -> U.ChCords -> String -> String
 subs1 t cc (x:y:[])
-  | not (C.isAlpha x && C.isAlpha y) || linx > 5 || liny > 5 = x : y : []
+  | not (C.isAlphaNum x && C.isAlphaNum y) || linx > 6 || liny > 6 = x : y : []
   | linx == liny = rightx : [righty]
   | colx == coly = underx : [undery]
   | otherwise = (t !! linx) !! coly : [(t !! liny) !! colx]
   where
-    rightx = (t !! linx) !! (mod (colx + 1) 5)
-    righty = (t !! liny) !! (mod (coly + 1) 5)
-    underx = (t !! (mod (linx + 1) 5)) !! colx
-    undery = (t !! (mod (liny + 1) 5)) !! coly
+    rightx = (t !! linx) !! (mod (colx + 1) 6)
+    righty = (t !! liny) !! (mod (coly + 1) 6)
+    underx = (t !! (mod (linx + 1) 6)) !! colx
+    undery = (t !! (mod (liny + 1) 6)) !! coly
     (linx, colx) = cc M.! x
     (liny, coly) = cc M.! y
 
@@ -44,14 +44,14 @@ encript _ [] = []
 encript k str = substitui criptTable (U.makeCords criptTable) filteredStr
   where
     criptTable =
-      U.makeTable k 5 5 $ L.nub $ iToJ (L.nub k) ++ ['A' .. 'H'] ++ ['J' .. 'Z']
-    filteredStr = toPairs $ filter (C.isLetter) $ iToJ str
+      U.makeTable k 6 6 $ L.nub $ L.nub k ++ ['A' .. 'Z'] ++ ['0' .. '9']
+    filteredStr = toPairs $ filter (C.isAlphaNum) str
 
 decript :: String -> String -> String
 decript _ [] = []
 decript k str = substitui deCriptTable (U.makeCords deCriptTable) filteredStr
   where
     deCriptTable =
-      U.makeTable k 5 5 $
-      reverse $ L.nub $ iToJ (L.nub k) ++ ['A' .. 'H'] ++ ['J' .. 'Z']
-    filteredStr = toPairs $ filter (C.isLetter) $ iToJ str
+      U.makeTable k 6 6 $
+      reverse $ L.nub $ L.nub k ++ ['A' .. 'Z'] ++ ['0' .. '9']
+    filteredStr = toPairs $ filter (C.isAlphaNum) str
