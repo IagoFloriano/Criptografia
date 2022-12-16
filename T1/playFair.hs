71a4b78b5062836e2c40cdb24d87f8fd811bd204
Iago Floriano <iagomfloriano@gmail.com>
2022-12-16 16:07:41 -0300

    PF 6x6

--- T1/playFair.hs
+++ T1/playFair.hs
@@ -21,27 +21,32 @@ toPairs (x:y:xs)
   | otherwise = [x : "X"] ++ toPairs (y : xs)
 toPairs (x:[]) = [x : "X"]
 
-subs1 :: U.Table -> U.ChCords -> String -> String
-subs1 t cc (x:y:[])
+subs1 :: U.Table -> U.ChCords -> String -> Int -> String
+subs1 t cc (x:y:[]) d
   | not (C.isAlphaNum x && C.isAlphaNum y) || linx > 6 || liny > 6 = x : y : []
   | linx == liny = rightx : [righty]
   | colx == coly = underx : [undery]
-  | otherwise = (t !! linx) !! coly : [(t !! liny) !! colx]
+  | otherwise = (t !! newlinx) !! newcoly : [(t !! newliny) !! newcolx]
   where
-    rightx = (t !! linx) !! (mod (colx + 1) 6)
-    righty = (t !! liny) !! (mod (coly + 1) 6)
-    underx = (t !! (mod (linx + 1) 6)) !! colx
-    undery = (t !! (mod (liny + 1) 6)) !! coly
+    rightx = (t !! newlinx) !! (mod (newcolx + 1) 6)
+    righty = (t !! newliny) !! (mod (newcoly + 1) 6)
+    underx = (t !! (mod (newlinx + 1) 6)) !! newcolx
+    undery = (t !! (mod (newliny + 1) 6)) !! newcoly
+    newlinx = mod (linx + d) 6
+    newliny = mod (liny + d) 6
+    newcolx = mod (colx + d) 6
+    newcoly = mod (coly + d) 6
     (linx, colx) = cc M.! x
     (liny, coly) = cc M.! y
 
-substitui :: U.Table -> U.ChCords -> [String] -> String
-substitui _ cc [] = []
-substitui t cc (x:xs) = subs1 t cc x ++ substitui t cc xs
+substitui :: U.Table -> U.ChCords -> [String] -> Int -> String
+substitui _ cc [] _ = []
+substitui t cc (x:xs) desloc =
+  subs1 t cc x desloc ++ substitui t cc xs (mod (desloc + 1) 6)
 
 encript :: String -> String -> String
 encript _ [] = []
-encript k str = substitui criptTable (U.makeCords criptTable) filteredStr
+encript k str = substitui criptTable (U.makeCords criptTable) filteredStr 0
   where
     criptTable =
       U.makeTable k 6 6 $ L.nub $ L.nub k ++ ['A' .. 'Z'] ++ ['0' .. '9']
@@ -49,7 +54,7 @@ encript k str = substitui criptTable (U.makeCords criptTable) filteredStr
 
 decript :: String -> String -> String
 decript _ [] = []
-decript k str = substitui deCriptTable (U.makeCords deCriptTable) filteredStr
+decript k str = substitui deCriptTable (U.makeCords deCriptTable) filteredStr 0
   where
     deCriptTable =
       U.makeTable k 6 6 $
